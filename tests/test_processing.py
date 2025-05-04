f72ee2acd26e8d377ed8e965ffb23ea4cb056bad
Ekaterina Gumina <eas9506760668@yandex.ru>
2025-05-04 18:38:48 +0300

    Add tests for processing module

---
+++ tests/test_processing.py
@@ -0,0 +1,30 @@
+import pytest
+from src.processing import filter_by_state, sort_by_date
+
+"""Тестирует корректность работы функции сортировки по значению state,
+проверяет сортировку по значению 'EXECUTED', 'CANCELED', если значение не передано"""
+
+def test_filter_by_state(filter_state):
+    assert filter_by_state(filter_state, state = 'EXECUTED') == [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+
+def test_filter_by_state(filter_state):
+    assert filter_by_state(filter_state, state = 'CANCELED') == [{"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}]
+
+
+def test_filter_by_state(filter_state):
+    assert filter_by_state(filter_state, state = '') == []
+
+"""Тестирует работу функции сортировки списка словарей 
+по датам в порядке убывания и возрастания,
+"""
+
+def test_sort_by_date(filter_state):
+    assert sort_by_date(filter_state, order_for_sort = True) == [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+
+
+def test_sort_by_date(filter_state):
+    assert sort_by_date(filter_state, order_for_sort = False) == [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}, {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}]
+
